cc48653da3d92f4f2b0a1c0dd7a2b99c2e4f4a26
Gilles Vico <gvico@w-r9-p7.s19.be>
2018-10-25 14:05:37 +0200

    Added files from the 24 oct. exam + NOTE.md

---
+++ exam-basedir/NOTE.md
@@ -0,0 +1,6 @@
+# NOTE
+
+This folder contains the programs i successfully did for exams, as well as their subjects.
+
+PLEASE NOTE that there's a high possibility for the programs contained here to be AWFULLY written, but hey, they work.
+I will probably try to optimize them in a (i hope) not-so-distant future, and they will be less eye-hurting.
---
+++ exam-basedir/rendu/tab_mult/tab_mult.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tab_mult.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/24 10:47:18 by exam              #+#    #+#             */
+/*   Updated: 2018/10/24 11:47:07 by exam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+	char buf;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		buf = str[i];
+		write(1, &buf, 1);
+		i++;
+	}
+}
+
+int		ft_pow10(const int nb)
+{
+	int puiss;
+	int nbr = nb;
+
+	puiss = 1;
+	while (nbr > 0)
+	{
+		puiss *= 10;
+		nbr--;
+	}
+	return (puiss);
+}
+
+int		cus_atoi(const char *str)
+{
+	int nbr;
+	int len;
+	int i;
+
+	nbr = 0;
+	len = 0;
+	i = 0;
+	while (str[len + 1] != '\0')
+		len++;
+	while (i <= len)
+	{
+		nbr += (str[i] - '0') * ft_pow10(len - i);
+		i++;
+	}
+	return (nbr);
+}
+
+int		ft_numlen(const int nb)
+{
+	int len;
+	int nbr = nb;
+
+	len = 0;
+	while (nbr > 0)
+	{
+		nbr /= 10;
+		len++;
+	}
+	return (len);
+}
+
+void	cus_itoa(const int nb)
+{
+	int nbr = nb;
+	int	len = ft_numlen(nbr) - 1;
+	char buf;
+
+	while (len >= 0)
+	{
+		buf = (nbr / ft_pow10(len)) + '0';
+		write(1, &buf, 1);
+		nbr -= (nbr / ft_pow10(len)) * ft_pow10(len);
+		len--;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int i;
+	int nb;
+
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	i = 1;
+	nb = cus_atoi(argv[1]);
+	while (i <= 9)
+	{
+		cus_itoa(i);
+		ft_putstr(" x ");
+		cus_itoa(nb);
+		ft_putstr(" = ");
+		cus_itoa(nb * i);
+		ft_putstr("\n");
+		i++;
+	}
+
+	return (0);
+}

21c90fdd7a348a64002acf013854e4cfc155b794
Gilles Vico <gvico@w-r6-p7.s19.be>
2018-11-30 13:36:29 +0100

    Updated Get_Next_Line.c

--- 0-Current_Projects/Get_Next_Line/get_next_line.c
+++ 0-Current_Projects/Get_Next_Line/get_next_line.c
@@ -6,30 +6,43 @@
 /*   By: gvico <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/14 11:03:18 by gvico             #+#    #+#             */
-/*   Updated: 2018/11/28 13:46:55 by gvico            ###   ########.fr       */
+/*   Updated: 2018/11/30 13:33:48 by gvico            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	get_next_line(const int fd, char **line)
+char	*read_fd(char *line, char *rest)
+{
+	char		*buf;
+	int			i;
+
+	if ((buf = ft_strnew(BUFF_SIZE + 1)) == NULL)
+		return (NULL);
+	while ()
+	{
+		i = 0;
+		ft_strclr(buf);
+		if ((read(fd, buf, BUFF_SIZE)) == -1)
+			return (NULL);
+		while (buf[i] != '\n' && buf[i] != '\0' && buf[i] != EOF)
+			i++;
+		ft_strncat(line, buf, i);
+		// ft_strsub ?
+	}
+}
+
+int		get_next_line(const int fd, char **line)
 {
 	int			i;
-	char		buf[BUFF_SIZE + 1];
+	// char		buf[BUFF_SIZE + 1];
 	static char	rest[BUFF_SIZE + 1];
 
-	if (fd == -1 || read(fd, buffer, 0) < 0 || !line)
+	if (fd == -1 || read(fd, buf, 0) < 0 || !line)
 		return (-1);
 	if (*rest)
-		ft_strcpy(*line, rest);
-/*
-	i = 0;
-	read(fd, buf, BUFF_SIZE);
-	while (buf[i] != '\n' && i < BUFF_SIZE)
 	{
-		**line = buf[i];
-		i++;
-		*line++;
+		ft_strcat(*line, rest);
+		ft_strclr(rest);
 	}
-*/
 }

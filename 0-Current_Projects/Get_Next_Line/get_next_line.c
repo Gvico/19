0c89db54051526fa5b09e84f49e609ba45e15969
Gilles Vico <gvico@e-r4-p7.s19.be>
2019-02-14 12:55:07 +0100

    Little update

--- 0-Current_Projects/Get_Next_Line/get_next_line.c
+++ 0-Current_Projects/Get_Next_Line/get_next_line.c
@@ -6,54 +6,53 @@
 /*   By: gvico <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 11:55:02 by gvico             #+#    #+#             */
-/*   Updated: 2019/02/08 13:55:34 by gvico            ###   ########.fr       */
+/*   Updated: 2019/02/14 12:33:38 by gvico            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static t_list	*get_file(t_list **file, int fd)
+static t_list	*get_file(t_list **files, int fd)
 {
-	t_list			*lst;
+	t_list			*cur;
 
-	lst = *file;
-	while (lst)
+	cur = *files;
+	while (cur)
 	{
-		if ((int)lst->content_size == fd)
-			return (lst);
-		lst = lst->next;
+		if ((int)cur->content_size == fd)
+			return (cur);
+		cur = cur->next;
 	}
-	lst = ft_lstnew("\0", fd);
-	ft_lstadd(file, lst);
-	lst = *file;
-	return (lst);
+	cur = ft_lstnew("\0", fd);
+	ft_lstadd(files, cur);
+	cur = *files;
+	return (cur);
 }
 
 int				get_next_line(const int fd, char **line)
 {
 	int				i;
-	int				ret;
 	char			buf[BUFF_SIZE + 1];
-	static t_list	*file;
-	t_list			*curr;
+	static t_list	*files;
+	t_list			*cur;
 
 	if (fd < 0 || !line || read(fd, buf, 0) < 0)
 		return (-1);
-	curr = get_file(&file, fd);
+	cur = get_file(&files, fd);
 	MEMCHK((*line = ft_strnew(1)));
-	while ((ret = read(fd, buf, BUFF_SIZE)))
+	while ((i = read(fd, buf, BUFF_SIZE)))
 	{
-		buf[ret] = '\0';
-		MEMCHK((curr->content = ft_strjoin(curr->content, buf)));
+		buf[i] = '\0';
+		MEMCHK((cur->content = ft_strjoin(cur->content, buf)));
 		if (ft_strchr(buf, '\n'))
 			break ;
 	}
-	if (!ft_strlen(curr->content))
+	if (!ft_strlen(cur->content))
 		return (0);
-	i = ft_strcpyuntil(line, curr->content, '\n');
-	if (i < (int)ft_strlen(curr->content))
-		curr->content += (i + 1);
+	i = ft_strcpyuntil(line, cur->content, '\n');
+	if (i < (int)ft_strlen(cur->content))
+		cur->content += (i + 1);
 	else
-		ft_strclr(curr->content);
+		ft_strclr(cur->content);
 	return (1);
 }

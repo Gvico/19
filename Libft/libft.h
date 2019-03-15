3a30ff880cc1c62326c05e55ab06fceedf777796
Gilles Vico <gvico@w-r5-p7.s19.be>
2019-03-15 15:20:15 +0100

    [LIBFT] Renamed ft_intswp.c to ft_swap.c to match Piscine_reloaded + [GNL] Little compile.sh update

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gvico <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/13 11:20:38 by gvico             #+#    #+#             */
-/*   Updated: 2019/02/07 12:56:10 by gvico            ###   ########.fr       */
+/*   Updated: 2019/03/15 15:14:29 by gvico            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,7 @@ t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 int				ft_numlen(int n);
 int				ft_wordcount(char const *s, char c);
 void			ft_quicksort(int *tab, int start, int end);
-void			ft_intswp(int *i, int *j);
+void			ft_swap(int *a, int *b);
 int				ft_strcpyuntil(char **dst, char const *src, char c);
 
 #endif
